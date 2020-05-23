d77e73a765bb684777ae237f29f1aab77b0602cd
Alexandre Beauquel <31766247+abeauquel@users.noreply.github.com>
2020-05-22 21:01:46 -0400

    Update era.ml

--- era.ml
+++ era.ml
@@ -21,6 +21,7 @@ let rec filter_out g = function
       a::l1
   | a::l -> filter_out g l ;;
 
+(* Verifier si x est multiple de m et x!=m *) 
 let is_multiple x m = 
   if x=m then
     false
@@ -29,17 +30,15 @@ let is_multiple x m =
       0 -> true
     | _ -> false;;
 
+(*Suprression des multiples d'une liste*)
 let remove_multiple n list=filter_out (function m -> (is_multiple m n)=false ) list;;
 
-
-let list = create_list 1 100;;
-
-
+(* fonction auxilière du crible d'eratosthene*)
 let rec aux max n list = match (List.nth list n * List.nth list n ) < max with
     true -> aux max (n+1)(remove_multiple (List.nth list n) list) 
   | false -> list;;
 
-  
+(* fonction du crible d'eratosthene*)
 let sieve max = aux max 0 (create_list 2 max);;
 
 sieve 1000
